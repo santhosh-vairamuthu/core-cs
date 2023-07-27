088c9f57157a177c394d31b9cab9a08cd1990bc2
santhosh-vairamuthu <santhosh22032004@gmail.com>
2023-07-27 17:51:15 +0000

    key generation success

--- cns/SDES.java
+++ cns/SDES.java
@@ -1,7 +1,7 @@
 // NOT YET COMPLETED
 import java.util.*;
 class SDES{
-    public static String initialPermutation(char[] txt){
+    public static String initialPermutation10(char[] txt){
         //Rearrange this according to your wish
         int[] arr={2,4,1,6,3,9,0,8,7,5};
         String text = "";
@@ -10,36 +10,36 @@ class SDES{
         }
         return text;
     }
+    public static String ePermutation10(char[] txt){
+        //Rearrange this according to your wish
+        int[] arr = {5,2,6,3,7,4,9,8};
+        String text = "";
+        for(int i=0;i<arr.length;i++){
+            text += txt[arr[i]];
+        }
+        return text;
+    }
     public static String shift(char[] txt, int x) {
         while (x > 0) {
             x--;
             String text = "";
-            int sp = txt.length / 2;
-            for (int i = 1; i < sp; i++) {
+            for (int i = 1; i < txt.length / 2; i++) {
                 text += txt[i];
             }
             text += txt[0];
-            for (int i = sp; i < sp * 2; i++) {
+            for (int i = (txt.length / 2)+1; i < txt.length; i++) {
                 text += txt[i];
             }
-            text += txt[sp];
+            text += txt[txt.length / 2];
             txt = text.toCharArray();
         }
         return new String(txt);
     }
     
-    public static String ePermutation(char[] txt){
-        //Rearrange this according to your wish
-        int[] arr = {5,2,6,3,7,4,9,8};
-        String text = "";
-        for(int i=0;i<txt.length;i++){
-            text += txt[arr[i]];
-        }
-        return text;
-    }
+    
     public static void encrypt(char[] text, char[] key){
-        String key1 = ePermutation(shift(initialPermutation(key).toCharArray(),1).toCharArray());
-        String key2 = ePermutation(shift(initialPermutation(key).toCharArray(),3).toCharArray());
+        String key1 = ePermutation10(shift(initialPermutation10(key).toCharArray(),1).toCharArray());
+        String key2 = ePermutation10(shift(initialPermutation10(key).toCharArray(),3).toCharArray());
         System.out.println(key1);
         System.out.println(key2);
     }
@@ -53,7 +53,6 @@ class SDES{
         String text = s.nextLine();
         System.out.print("Enter a key : ");
         String key = s.nextLine();
-        s.nextLine();
         if (ch == 1) {
             // System.out.println("Cipher Text : " + encrypt(text.toCharArray(), key.toCharArray()));
             encrypt(text.toCharArray(), key.toCharArray());
